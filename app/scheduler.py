538f289263c1c18aacfd40e50bfa5d4e71c6074b
UM DAVIN <47536231+udabin@users.noreply.github.com>
2025-04-21 14:11:15 +0900

    Update scheduler.py

--- app/scheduler.py
+++ app/scheduler.py
@@ -8,14 +8,6 @@ import os
 
 def sync_job():
     print("스케줄러가 자동으로 캘린더를 동기화합니다...")
-    # try:
-    #     response = requests.get("http://127.0.0.1:8000/sync")
-    #     if response.status_code == 200:
-    #         print("캘린더 동기화 성공:", response.json())
-    #     else:
-    #         print("캘린더 동기화 실패:", response.text)
-    # except Exception as e:
-    #     print("스케줄러 오류:", e)
     try :
         events = get_upcoming_events()
         print(f"불러온 이벤트 개수: {len(events)}개")
